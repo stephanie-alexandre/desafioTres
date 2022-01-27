bf9a2034487a42487ee2b9b415e3b36771aa425f
stephanie-alexandre <gomesstephanie976@gmail.com>
2022-01-26 23:39:11 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+for (let i=1;i<=10;i++){
+    console.log(i)
+}
+
+
+
+let conta= 10;
+while (1<=conta){
+    console.log (conta)
+    conta--
+}
+
+
+
+let contador= 1;
+while(100>=contador){
+    console.log(contador)
+    contador +=2;
+}
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+ for (let cem= 0; cem <=100; cem +=2){
+     console.log(cem)
+ }
